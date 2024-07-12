624997c39e50e83b2de440e389080a91d9909500
Akshidhan <wakshiwije@gmail.com>
2024-07-12 13:36:51 -0400

    Finished adding time feature

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,9 +3,11 @@ const clearbtn = document.getElementById('clearBtn');
 const searchResult = document.getElementById('searchResult');
 
 function searchFunction(){
+    clearFunction();
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data=>{
+        var cityName;
         var search = document.getElementById('searchBar').value.toLowerCase();
         if (search == 'beach' || search == 'beaches'){
             search = 'beaches';
@@ -19,7 +21,6 @@ function searchFunction(){
         else{
             search = 'invalid';
         }
-        debugger
         if(search == 'countries'){
             data.countries.forEach((country, id) => {
                 country.cities.forEach((city, id) => {
@@ -34,11 +35,16 @@ function searchFunction(){
                                                 </div>`
                 })
             });
+            const options = { timeZone: 'America/Sao_Paulo', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+            const cityTime = new Date().toLocaleTimeString('en-US', options);
+            console.log("Current time in Sao Paulo, Brazil: " , cityTime);
+            document.getElementById('time').innerHTML = `Current time in <b>Sao Paulo, Brazil</b>:<br> ${cityTime}`
         }
         else if(search == 'beaches' || search == 'temples'){
             if (data[search] && Array.isArray(data[search])) {
                 data[search].forEach((city, id) => {
                     var name = city.name;
+                    cityName = name;
                     var image = city.imageUrl;
                     var description = city.description;
                     searchResult.style.display = 'block';
@@ -48,6 +54,18 @@ function searchFunction(){
                                                     <p class="description">${description}</p>
                                                 </div>`;
                 });
+                if (search == 'temples'){
+                    var timeZone = 'Asia/Kolkata';
+                    cityName = 'Taj Mahal, India';
+                }
+                else{
+                    var timeZone = 'Pacific/Tahiti';
+                    cityName = 'Copacabana beach, Frech Polynesia';
+                }
+                const options = { timeZone: timeZone, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                const cityTime = new Date().toLocaleTimeString('en-US', options);
+                console.log("Current time in ", cityName, ": " , cityTime);
+                document.getElementById('time').innerHTML = `Current time in <b>${cityName}</b>:<br> ${cityTime}`
             } else {
                 searchResult.style.display = 'block';
                 searchResult.innerHTML = `<p class="name">No result found!</p>`;
@@ -64,9 +82,11 @@ function searchFunction(){
 }
 
 function clearFunction(){
-    searchResult.innerHTML = "";
+    searchResult.innerHTML = `<div id="time"></div>`;
     searchResult.style.display = 'none';
 }
+
+
 
 searchbtn.addEventListener('click', searchFunction);
 clearbtn.addEventListener('click', clearFunction);
